43150c12d894372433477174f1369ca59a5be809
LatixXGod <79203041+LatixXGod@users.noreply.github.com>
2021-02-23 13:45:38 +0300

    Homework 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Привет,давай познакомимся? Как тебя зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}. Cегодня [{DateTime.Now}]");
+
+
+
+
+
+        }
+    }
+
+}
