3631fadbb57eee5e19c133c123e82cfaa963b3b5
LatixXGod <79203041+LatixXGod@users.noreply.github.com>
2021-02-25 18:29:08 +0300

    Homework 2-3

---
+++ HomeWork 2-3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Homework_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число чтобы узнать четное ли оно");
+            string i = Console.ReadLine();
+            try
+            {
+                int i2 = Convert.ToInt32(i);
+                if (i2 % 2 == 0)
+                {
+                    Console.WriteLine("Ваше число кратно двум, ваше число было - " + i2);
+                }
+                else
+                {
+                    Console.WriteLine("Ваше число не кратно двум" );
+                }
+
+
+
+
+
+
+
+
+
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Введите числовое значение");
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
+    
+    
+
+       
