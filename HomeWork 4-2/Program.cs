7e4a5acf062ff2e6814252eb9a547f9933d59cb2
LatixXGod <79203041+LatixXGod@users.noreply.github.com>
2021-03-08 20:51:11 +0300

    Homework 2

---
+++ HomeWork 4-2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HomeWork_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числовое значение через пробел:");
+            string lul = Console.ReadLine();
+            Console.WriteLine(juj(lul));
+        }       
+        static int juj(string lul)
+        {           
+            string temp = "";
+            int sum = 0;
+            for (int i = 0; i < lul.Length; i++)
+            {
+                char Ver = lul[i];
+                if (char.IsDigit(Ver))
+                {
+                    temp += Ver;
+                }
+                else
+                {
+                    sum += int.Parse(temp);
+                    temp = "0";
+                }
+               
+            }
+            Console.WriteLine("Ваша сумма чисeл:");
+            return sum + int.Parse(temp);
+            
+
+        } 
+        
+    }
+}
+
+
+
+
+
+//Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом
+//возвращающую число — сумму всех чисел в строке. Ввести данные с клавиатуры и вывести результат на экран.
