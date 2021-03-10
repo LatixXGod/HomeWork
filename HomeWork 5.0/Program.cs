ba9718e93e3c68e35e075532ae0cad652285fc23
LatixXGod <79203041+LatixXGod@users.noreply.github.com>
2021-03-10 21:49:42 +0300

    HomeWork 5-2

---
+++ HomeWork 5.0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Data;
+
+namespace HomeWork_5_1
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            string filename = "startup.txt";          
+            File.WriteAllText(path: filename, contents: "Актуальное время - " + localDate);
+            File.AppendAllText(filename, Environment.NewLine);
+            File.AppendAllLines(filename, new[] { "-------------------------------------" });
+            File.AppendAllLines(filename, new[] { "Приятного времяпровождения"}); 
+
+        }
+
+    }
+}
