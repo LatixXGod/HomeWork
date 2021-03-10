575641aad17ea7745347b82b465cd429e75d5915
LatixXGod <79203041+LatixXGod@users.noreply.github.com>
2021-03-10 21:48:56 +0300

    HomeWork 5-1

---
+++ HomeWork 5.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace HomeWork_5_1
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            string filename = "Результат дз 5-1.txt";
+            File.WriteAllText(path: filename, contents: "Типо произвольный ввод данных");
+        }
+
+    }
+}
